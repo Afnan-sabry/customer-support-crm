49050110e1bcb1f814dd7817f50f72cffbbae91b
Afnan.Sabry <Afnan.Sabry@techprocess.net>
2026-09-01 10:09:15 +0300

    feat: add DI registration pattern and empty AppDbContext shell

---
+++ src/CustomerSupport.Domain/DependencyInjection.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CustomerSupport.Domain;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddDomainServices(this IServiceCollection services)
+    {
+        return services;
+    }
+}
---
+++ src/CustomerSupport.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerSupport.Infrastructure.Persistence;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
